9a0fd8e7735818056b7a9f695b4a9e83882e7117
Chuck Burgess <cdburgess@gmail.com>
2013-02-24 19:39:11 -0700

    Added the init manifest to install workbench.

---
+++ manifests/init.pp
@@ -0,0 +1,11 @@
+# Install MySQL Workbench into /Applications
+#
+# Usage:
+#
+#     include mysql_workbench 
+class mysql_workbench {
+  package { 'MySQLWorkbench':
+    provider => 'appdmg',
+    source   => 'http://cdn.mysql.com/Downloads/MySQLGUITools/mysql-workbench-gpl-5.2.47-osx-i686.dmg';
+  }
+}
