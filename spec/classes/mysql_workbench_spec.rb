7b07d455b2e9ff68e121820d2f40c3b2289b536c
Ryan T. Dean <rtdean@cytherianage.net>
2016-05-21 15:03:10 -0700

    Update tests

--- spec/classes/mysql_workbench_spec.rb
+++ spec/classes/mysql_workbench_spec.rb
@@ -4,7 +4,7 @@ describe 'mysql_workbench' do
   it do
     should contain_package('MysqlWorkbench').with({
       :provider => 'appdmg',
-      :source   => 'http://cdn.mysql.com/Downloads/MySQLGUITools/mysql-workbench-gpl-5.2.47-osx-i686.dmg'
+      :source   => 'http://cdn.mysql.com/Downloads/MySQLGUITools/mysql-workbench-community-6.2.5-osx-i686.dmg'
     })
   end
 end
